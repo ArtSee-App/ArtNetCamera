d570881e8198addbb9f3e953d3b104e83666d4dc
Vlad Stoenescu <64587367+vladstoe@users.noreply.github.com>
2023-12-28 18:26:00 +0200

    asd

--- server.py
+++
@@ -1,8 +0,0 @@
-from tensorflow import keras
-import tensorflowjs as tfjs
-
-def importModel(modelPath):
-    model = keras.models.load_model(modelPath)
-    tfjs.converters.save_keras_model(model, "tfjsmodel")
-
-importModel("./model")
